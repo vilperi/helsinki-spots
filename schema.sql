255d29cae02538e2f2de250c63f8ba2ec202e914
Vili Rauhala <vili.rauhala@helsinki.fi>
2025-03-17 15:14:10 +0200

    Add tables for spots and comments

--- schema.sql
+++ schema.sql
@@ -3,3 +3,20 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE spots (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    lat REAL,
+    lon REAL,
+    category TEXT
+    user_id INTEGER REFERENCES users
+);
+
+CREATE TABLE comments (
+    id INTEGER PRIMARY KEY,
+    content TEXT,
+    sent_at TEXT,
+    user_id INTEGER REFERENCES users,
+    spot_id INTEGER REFERENCES spots
+);
